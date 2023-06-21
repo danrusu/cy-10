e39cc3a1e524c34e1f3ddcda96d4dd2d02da2f0f
Dan Rusu <dangingko@yahoo.com>
2023-06-21 15:50:26 +0300

    async-sync + global var

---
+++ cypress/e2e/spec/async-sync.spec.js
@@ -0,0 +1,38 @@
+let customKey;
+
+function setCustomKey(val) {
+  customKey = val;
+}
+
+describe('Graphql test', () => {
+  beforeEach(() => {
+    cy.visit('cypress/graphql.html');
+  });
+
+  it('test1', () => {
+    cy.intercept('https://qatools.ro/gql', req => {
+      const id = req.body.variables.id;
+      req.reply({
+        statusCode: 200,
+        body: { id, key: 1234 },
+        delay: 100,
+      });
+    }).as('gql');
+
+    cy.get('#gql1').click();
+    cy.wait('@gql').then(xhr => {
+      setCustomKey(xhr.response.body.key);
+      console.log(customKey);
+    });
+
+    cy.wrap(null).then(() => {
+      console.log('test1: from wrap(null) context', customKey);
+    });
+  });
+
+  it('test2', () => {
+    cy.wrap(null).then(() => {
+      console.log('test2: from wrap(null) context', customKey);
+    });
+  });
+});
